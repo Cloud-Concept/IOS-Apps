b92d91d40898911893c3f9359508eb3e83773b60
George Hanna Adly <gadly@cloudconceptgroup.com>
2016-03-09 13:49:11 +0200

    added session

--- Demo/GV-Suggestion-Complaint/GV Suggestion & Complaint/ViewController.swift
+++ Demo/GV-Suggestion-Complaint/GV Suggestion & Complaint/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController,UIWebViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         let url = NSURL(string: "http://2015-gveservices.cs17.force.com/GVSupport/")
+        retrieveCookies()
         let request = NSURLRequest(URL: url!)
         webView.loadRequest(request)
     }
@@ -28,9 +29,33 @@ class ViewController: UIViewController,UIWebViewDelegate {
     override func shouldAutorotate() -> Bool {
         return false;
     }
+    
+    func saveCookies(){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let cookiesData = NSKeyedArchiver.archivedDataWithRootObject(NSHTTPCookieStorage.sharedHTTPCookieStorage().cookies!)
+        
+        defaults.setObject(cookiesData, forKey: "cookies")
+        defaults.synchronize()
+    }
+    func deleteCookies(){
+        NSUserDefaults.standardUserDefaults().removeObjectForKey("cookies")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    func retrieveCookies(){
+        if (NSUserDefaults.standardUserDefaults().objectForKey("cookies") != nil)
+        {
+            let cookies = NSKeyedUnarchiver.unarchiveObjectWithData(NSUserDefaults.standardUserDefaults().objectForKey("cookies") as! NSData) as! NSArray
+            let cookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+            for cookie in cookies
+            {
+                cookieStorage.setCookie(cookie as! NSHTTPCookie)
+            }
+        }
+    }
 
     func webViewDidFinishLoad(webView: UIWebView) {
         SVProgressHUD.dismiss()
+        saveCookies()
     }
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         return true;
--- Demo/Global Village Support/Global Village Support/ViewController.swift
+++ Demo/Global Village Support/Global Village Support/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController,UIWebViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         let url = NSURL(string: "http://gveservices.force.com/GVSupport/")
+        retrieveCookies()
         let request = NSURLRequest(URL: url!)
         webView.loadRequest(request)
     }
@@ -29,11 +30,35 @@ class ViewController: UIViewController,UIWebViewDelegate {
         return false;
     }
 
+    func saveCookies(){
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let cookiesData = NSKeyedArchiver.archivedDataWithRootObject(NSHTTPCookieStorage.sharedHTTPCookieStorage().cookies!)
+        
+        defaults.setObject(cookiesData, forKey: "cookies")
+        defaults.synchronize()
+    }
+    func deleteCookies(){
+        NSUserDefaults.standardUserDefaults().removeObjectForKey("cookies")
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+    func retrieveCookies(){
+        if (NSUserDefaults.standardUserDefaults().objectForKey("cookies") != nil)
+        {
+            let cookies = NSKeyedUnarchiver.unarchiveObjectWithData(NSUserDefaults.standardUserDefaults().objectForKey("cookies") as! NSData) as! NSArray
+            let cookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+            for cookie in cookies
+            {
+                cookieStorage.setCookie(cookie as! NSHTTPCookie)
+            }
+        }
+    }
+    
     func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
         SVProgressHUD.dismiss()
     }
     func webViewDidFinishLoad(webView: UIWebView) {
         SVProgressHUD.dismiss()
+        saveCookies()
     }
     func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         return true;
